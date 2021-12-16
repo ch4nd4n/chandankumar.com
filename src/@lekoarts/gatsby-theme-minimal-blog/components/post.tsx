3f7e3369e032c31508d77d103e2b2f55d4f4cf23
Chandan Kumar <chandankumar.r@gmail.com>
2021-12-16 19:15:59 +0530

    Add Firebase Gatsby integration

--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout";
 import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags";
 import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo";
-import { Disqus } from "gatsby-plugin-disqus";
+import Comment from "./comments";
 
 type PostProps = {
   data: {
@@ -71,6 +71,7 @@ const Post = ({ data: { post } }: PostProps) => (
       }}
     >
       <MDXRenderer>{post.body}</MDXRenderer>
+      <Comment slug={post.slug} />
     </section>
     {/* <Disqus config={{ identifier: post.slug, title: post.title }} /> */}
   </Layout>
